effe55db60d66a2460c86f32cf9c9735ac03b3c4
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-13 10:09:12 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-/** 
+
+/**
  * main - Entry point
- * 
+ *
  * Discription: A C program that prints with put function
  *
  * Return: Always 0 (Success)
@@ -9,6 +10,6 @@
 
 int main(void)
 {
-           puts("\"Programming is like building a multilingual puzzle");
-           return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
