b14a6d4a4a1c907a77b76dfdfa72d71a68130019
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-13 10:02:26 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,9 @@
  * Discription: A C program that prints with put function
  *
  * Return: Always 0 (Success)
- */
+*/
 
-
-int main()
+int main(void)
 {
            puts("\"Programming is like building a multilingual puzzle");
            return 0;
