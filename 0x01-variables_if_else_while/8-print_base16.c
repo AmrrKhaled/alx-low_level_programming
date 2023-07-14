1eb9c396b1bdaf7f25b6dfb7d99ca6878e094fe5
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-14 11:47:24 -0700

    C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that prints the number is positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x = 0;
+	char ch = 'a';
+	
+	while (x < 10)
+	{
+	putchar(48 + x);
+	x++;
+	}
+	while (ch <= 'f')
+	{
+	putchar(ch);
+	ch++;
+	}
+	putchar('\n');
+	return (0);
+}
