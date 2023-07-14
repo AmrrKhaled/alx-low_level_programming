081ef723262b338ad6022c75dc84a30342e01149
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-14 11:15:25 -0700

    C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that prints the number is positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x = '0';
+
+	while (ch <= '9')
+	{
+	printf("%d", x);
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
