bb3d070381d7464e195f8a532f9b318052d4ad7d
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-14 17:41:37 -0700

    C

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that prints the number is positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x = 48, y = 48;
+	int i = 0;
+
+	while (x < 58)
+	{
+	i++;
+	while (y < 58)
+	{
+	if (x > y)
+	{
+	putchar(y);
+	putchar(x);
+	if (!(x == '8' && y == '9'))
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	else if (x < y)
+	{
+	putchar(x);
+	putchar(y);
+	if (!(x == '8' && y == '9'))
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	y++;
+	}
+	x++;
+	y = 48 + i;
+	}
+	return (0);
+
+}
