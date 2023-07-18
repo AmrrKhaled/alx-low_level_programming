aa477a995347c89a38286595fa769f0579b14578
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 08:07:59 -0700

    C

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet-  utilizes on the _putchar function to print
+ * the alphabet from a-z 10 times
+*/
+void print_alphabet_x10(void);
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
+
+}
+
