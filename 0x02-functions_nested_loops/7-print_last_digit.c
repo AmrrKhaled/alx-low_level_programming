4230eef1aabd9cb424c204c5791dcb843a2212c2
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 09:00:47 -0700

    C

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit-function  check the number and return its last digit
+ *
+ * @n: check the input of function
+ *
+ * Return: retuns the last digit of a number
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
