caada19783a3fa1b87727194fbc60332051632f0
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 10:33:42 -0700

    C

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table-function prints nine times the number from 0 to 9 with increment
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	if (j > 0)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	int value = i * j;
+
+	if (value < 10)
+	_putchar(' ');
+	if (value < 10)
+	{
+	_putchar(' ');
+	_putchar(value + '0');
+	{
+	else
+	{
+	_putchar(value / 10 + '0');
+	_putchar('.');
+	_putchar(value % 10 + '0');
+	}
+	}
+	_putchar('\n');
+	}
+}
