86081be657636d0bebc9a057e22ea382c36ac59a
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 08:46:36 -0700

    C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * print_sign:  check the number positve or negative
+ * print_sign -  check the number positve or negative or zero
+ * and print the sign + or - or 0
  *
  * @n: check the input of function
  *
- * Return: retuns 1 if c is positive otherwise alwayzi 0 (success)
+ * Return: retuns 1 if + 0 if 0 returns -1 if -
 */
 int print_sign(int n)
 {
@@ -17,12 +18,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-	_putchar(0);
+	_putchar(48);
 	return (0);
 	}
 	else
 	{
 	_putchar('-');
-	return (0);
+	return (-1);
 	}
 }
