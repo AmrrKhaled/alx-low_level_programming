9648a700a37223651d16928025f3a586ff4ee336
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 11:46:08 -0700

    C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add-function  check the two inputs and return the addition
+ *
+ * @x,@y: check the input of function
+ *
+ * Return: retuns 1 if c is lower otherwise alwayzi 0 (success)
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
