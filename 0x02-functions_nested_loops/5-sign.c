d1c0d62bd0c49306a31897bc68df7b05e1555030
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 08:38:23 -0700

    C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower-function  check the character is uppercase or smallercase
+ *
+ * @c: check the input of function
+ *
+ * Return: retuns 1 if c is lower otherwise alwayzi 0 (success)
+*/
+int _isalpha(int c)
+{
+	if(n>0)
+	{
+	_putchar('+')
+	return (1);
+	}
+	else if(n=0)
+	{
+	_putchar(0)
+	return (0);
+	}
+	else 
+	{
+	_putchar('-')
+	return (0);
+	}
+}
