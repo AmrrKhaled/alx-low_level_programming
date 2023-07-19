8ceca43f0ec9a5a1f9a2692d7c821f8870cfcb3a
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 19:47:31 -0700

    C

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include "main.h"
-int main() {
-    int prev = 1;
-    int current = 2;
-    int next_term = 0;
-    int sum = 2;
-    while (next_term <= 4000000) {
-        next_term = prev + current;
-        prev = current;
-        current = next_term;
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that prints the number is positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int prev = 1;
+	int current = 2;
+	int next_term = 0;
+	int sum = 2;
 
-        if (next_term % 2 == 0) {
-            sum += next_term;
-        }
-    }
-
-    printf("%d\n", sum);
-
-    return 0;
+	while (next_term <= 4000000)
+	{
+	next_term = prev + current;
+	prev = current;
+	current = next_term;
+	if (next_term % 2 == 0)
+	sum += next_term;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
