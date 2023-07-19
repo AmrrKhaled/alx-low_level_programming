447cb24d8e84a3375e2b2e3bf05fef6b427bb455
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 19:02:06 -0700

    C

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table-function prints nine times the number from 0 to 9 with increment
+*/
+int main(void)
+{
+	int sum = 0, i;
+	for (i = 0; i < 1024; i++){
+	if ( i% 3== 0)
+	sum += i;
+	if(i % 5 == 0)
+	sum += i;
+	}
+	printf("%d\n",sum);
+}
