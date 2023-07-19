84fc5bfa385a9161d3aea676f6eaca751385a5bc
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 18:02:01 -0700

    C

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,22 +8,23 @@
  * @n: check the input of function
 */
 void print_to_98(int n){
-	int i ;
+int i ;
 if(n<98)
 {
-    for (i = n ; i <= 98; i++)
+    for (i = n ; i < 98; i++)
     {
         printf("%d, ",i);
         }
         }
 else if(n>98){
 
-     for (i = n ; i >= 98; i--)
+     for (i = n ; i > 98; i--)
     {
         printf("%d, ",i);
         }
 }
 
 printf("98");
+
 
 }
