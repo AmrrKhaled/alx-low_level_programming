277281855ec9b6c69e6913709eb193645f07d841
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 19:25:51 -0700

    C

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: A C program that prints the number is positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void) {
+   long unsigned f1=1;
+   long unsigned f2=1;
+   long unsigned f3;
+  for(int i=0; i<50;i++)
+  {
+    printf("%lu",f1);
+    if(i<49)
+    printf(", ");
+    f3=f1+f2;
+    f1=f2;
+    f2=f3;
+ }
+return (0);
+}
