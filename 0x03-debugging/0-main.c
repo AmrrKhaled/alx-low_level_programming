ff4a2e566d54fd8da17721f17c04cc347ed800ba
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-19 07:02:08 -0700

    C

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Discription: A C program that prints the number is positive or negative
- *
- * Return: Always 0 (Success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	printf("%d is negative\n", n);
-	else if (n > 0)
-	printf("%d is positive\n", n);
-	else
-	printf("%d is zero\n", n);
+	i = 98;
+	positive_or_negative(i);
+
 	return (0);
 }
