b38fcb2519de398cf2800356a455a0ded9fc8b38
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 19:44:26 -0700

    C

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-nclude "main.h"
+#include "main.h"
 int main() {
     int prev = 1;
     int current = 2;
     int next_term = 0;
-    int sum = 2; // Start with sum = 2 because 2 is the second even-valued term in the Fibonacci sequence
-
+    int sum = 2;
     while (next_term <= 4000000) {
         next_term = prev + current;
         prev = current;
