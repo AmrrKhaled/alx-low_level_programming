66aeb805e3a26ca40b77a51958d7de8365ee2339
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-18 19:08:37 -0700

    C

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,17 +2,23 @@
 #include <stdio.h>
 
 /**
- * times_table-function prints nine times the number from 0 to 9 with increment
+ *  main - Entry point
+ *
+ *  Discription: A C program that prints the number is positive or negative
+ *
+ *  Return: Always 0 (Success)
 */
 int main(void)
 {
 	int sum = 0, i;
-	for (i = 0; i < 1024; i++){
-	if ( i% 3== 0)
+
+	for (i = 0; i < 1024; i++)
+	{
+	if (i % 3 == 0)
 	sum += i;
-	if(i % 5 == 0)
+	else if (i % 5 == 0)
 	sum += i;
 	}
-	printf("%d\n",sum);
-	return 0;
+	printf("%d\n", sum);
+	return (0);
 }
