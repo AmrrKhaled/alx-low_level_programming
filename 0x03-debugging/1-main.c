c995a3b2e2219ff273bfcb7fe6f814ee506bd88c
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-19 07:11:50 -0700

    C

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,10 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-	while (i < 10)
-/* the i value doesnt change so it will be always less than 10 */
-	{
-	putchar(i);
-	}
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
