b6d0a7e97ecdc39c9ed2525c2fde9c10728d8df8
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-20 10:41:36 -0700

    C

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j z;
+
+	if (n <= 0)
+	_putchar('\n');
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+	for (j = 0; j < n - i; j++)
+	_putchar(' ');
+	for (z = n; z >= n - i; z--)
+	_putchar('#');
+	_putchar('\n');
+	}
+	}
+	return (0);
+}
