93ff28b177d5113a4d8b5c49d7200d97bf37567a
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-20 08:01:21 -0700

    C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul-function  check the two inputs and return the addition
+ *
+ * @x: check the input of function
+ * @y: check the second input
+ * Return: retuns 1 if c is lower otherwise alwayzi 0 (success)
+*/
+int mul(int x, int y)
+{
+	return (x * y);
+}
