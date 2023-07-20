7d884c283e4aa793a6e84de4c498e3093cee39c8
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-20 09:16:52 -0700

    C

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line -  function takes input n and prints this \ n times and the \n if
+ * n is 0 or negative prints \n only
+ *
+ * @n: check the input of the function
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	_putchar('\n');
+	else
+	{
+	for (i = 0; i < n; i++)
+	{
+	_putchar('\');
+	}
+	_putchar('\n');
+	}
+}
