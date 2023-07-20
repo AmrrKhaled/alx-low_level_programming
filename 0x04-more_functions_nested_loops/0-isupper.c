79d906c8e531f2d43ba06e56afb6c916240dcc3b
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-20 07:49:48 -0700

    C

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper-function  check the character is uppercase or smallercase
+ *
+ * @c: check the input of function
+ *
+ * Return: retuns 1 if c is upper otherwise alwayzi 0 (success)
+*/
+int _isupper(int c)
+{
+	if (c <= 97 && c >= 65)
+	return (1);
+	else
+	return (0);
+}
