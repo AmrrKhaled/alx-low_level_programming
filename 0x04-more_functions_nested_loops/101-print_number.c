c75bd11b80169136e0271162dbbd0b5563f75e3c
AmrrKhaled <amrkhaledsaada16@gmail.comgit config --global user.name Your>
2023-07-20 17:55:23 -0700

    C

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	if (abs(n) < 10)
+	putchar(n % 10 + 48);
+	else if (abs(n) < 100)
+	{
+	putchar(n / 10 + 48);
+	putchar(n % 10 + 48);
+	}
+	else if (abs(n) < 1000)
+	{
+	putchar(n / 100 + 48);
+	putchar((n / 10) % 10 + 48);
+	putchar(n % 10 + 48);
+	}
+	else if (abs(n) < 10000)
+	{
+	putchar(n / 1000 + 48);
+	putchar((n / 10) % 10 + 48);
+	putchar((n / 10) % 10 + 48);
+	putchar(n % 10 + 48);
+	}
+	return (0);
+}
